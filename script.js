5501e5f99a7fab2632c1f151a0e8a49c162f2ed5
leornungcniht <joseph.hughes@svu.edu>
2024-04-11 10:37:42 -0400

    slideshow and more

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function showpage(button, newpage) {
-
   buttonid = button.id;
 
   allpages = document.getElementsByClassName("page show");
@@ -10,6 +9,30 @@ function showpage(button, newpage) {
   document.getElementById(newpage).className = "page show";
 }
 
-function changeImage {
-  
+const homeHero = document.getElementById("home-hero");
+
+function change1(div) {
+  div.style.backgroundImage = "url(images/andie-lemonade-top.jpg)";
 }
+
+function change2(div) {
+  div.style.backgroundImage = "url(images/andie-pink-lemonade-top.jpg)";
+}
+
+function change3(div) {
+  div.style.backgroundImage = "url(images/andie-chocolate-top.jpg)";
+}
+
+function slideshow() {
+  console.log("flag");
+  change1(homeHero);
+  setTimeout(function () {
+    change2(homeHero);
+    setTimeout(function () {
+      change3(homeHero);
+      setTimeout(slideshow, 5000);
+    }, 5000);
+  }, 5000);
+}
+
+slideshow();
