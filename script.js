de121c4f02934d4b0f56bedf675ab3b63e868d19
leornungcniht <joseph.hughes@svu.edu>
2024-04-20 13:10:59 -0400

    work moves forward

--- script.js
+++ script.js
@@ -39,14 +39,64 @@ slideshow();
 
 function showDescription(description) {
   document.getElementById('defaultDescription').style.display = "none";
-  const previousDescription = document.getElementsByClassName("description show")[0];
+  const previousDescription = document.getElementsByClassName("montserrat-regular description show")[0];
   if (previousDescription) {
-    previousDescription.className = "description no-show";
+    previousDescription.className = "montserrat-regular description no-show";
   }
 
-  document.getElementById(description).className = "description show";
+  document.getElementById(description).className = "montserrat-regular description show";
 }
 
 function addToCart(cookie) {
   console.log(cookie);
+  order[cookie] ++;
+  console.log(order);
+  toggleHidden('buy-counter');
+  updateCounter();
+  updateOrderSummary();
+  updateOrderTotal();
+}
+
+function toggleHidden(thing) {
+  if (document.getElementById(thing).className = "buy-counter no-show") {
+    document.getElementById(thing).className = "buy-counter show";
+  } else {
+    document.getElementById(thing).className = "buy-counter no-show";
+  }
+}
+
+function updateCounter() {
+  const counter = document.getElementById('buy-counter');
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+}
+
+const order = {
+  'Blueberry Twirl': 0,
+  'Chocolate Chip': 0,
+  'Chocolate': 0,
+  'Craisin': 0,
+  'Frosted Stripes': 0,
+  'Macadamia Nut': 0,
+  'Peanut Butter': 0,
+  'Snickerdoodle': 0
+}
+
+function updateOrderSummary() {
+  let orderSummary = '<br>';
+  for (let cookie in order) {
+    if (order[cookie] !== 0) {
+      orderSummary += `${cookie}: ${order[cookie]} <br>`;
+    }
+  }
+  document.getElementById('order-summary').innerHTML = orderSummary;
+}
+
+function updateOrderTotal() {
+  let orderTotal = 0.00;
+  for (let cookie in order) {
+    if (order[cookie] !== 0) {
+      orderTotal += 1.25;
+    }
+  }
+  document.getElementById('order-total').innerHTML = orderTotal;
 }
